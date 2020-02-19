5867819a287a219229c7294d1b443d781b7ade45
Evgeny Yakimov <john.yakimov@gmail.com>
2020-02-19 22:52:32 +0000

    Cross-platform windll

--- windll/app.c
+++ windll/app.c
@@ -1,17 +1,51 @@
+#include <string.h>
+
+#ifdef __linux__
+#include <dlfcn.h>
+
+typedef void* Handle;
+Handle open_lib(const char* filename) {
+  char basename[100] = "";
+  strcat(strcat(strcat(basename, "./lib"), filename), ".so");
+  return dlopen(basename, RTLD_NOW);
+}
+
+void* open_symbol(Handle handle, const char* symbol) {
+  return dlsym(handle, symbol);
+}
+
+#define CALLBACK
+#elif _WIN32
+
 #include <windows.h>
 
-typedef void (CALLBACK* DoStuff)();
+typedef HINSTANCE Handle;
+Handle open_lib(const char* filename) {
+  char basename[100] = "";
+  strcat(strcat(basename, filename), ".dll");
+  return LoadLibrary(basename);
+}
+
+void* open_symbol(Handle handle, const char* symbol) {
+  return GetProcAddress(handle, symbol);
+}
+
+#endif
+
+typedef void(CALLBACK* DoStuff)();
 
 int main() {
-    HINSTANCE libbInst = LoadLibrary ("libb.dll");
-    DoStuff libbDoStuff = (DoStuff) GetProcAddress(libbInst, "libbDoStuff");
+  Handle libbInst = open_lib("libb");
+//   puts(dlerror());
+  DoStuff libbDoStuff = (DoStuff)open_symbol(libbInst, "libbDoStuff");
+//   puts(dlerror());
 
-    HINSTANCE libcInst = LoadLibrary ("libc.dll");
-    DoStuff libcDoStuff = (DoStuff) GetProcAddress(libcInst, "libcDoStuff");
+  Handle libcInst = open_lib("libc");
+  DoStuff libcDoStuff = (DoStuff)open_symbol(libcInst, "libcDoStuff");
 
     libbDoStuff();
     libcDoStuff();
     libbDoStuff();
     libcDoStuff();
-    return 0;
+  return 0;
 }
